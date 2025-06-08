f7fedc24fe06a0692cf50bba4f999fa9bae8e13c
Emilio Correa <ehcorrea60@gmail.com>
2025-06-07 21:44:01 -0300

    refactor: update Button styles

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -1,15 +1,11 @@
-import styled, { css } from 'styled-components/native';
-import { Image, ImageProps } from 'expo-image';
+import styled, { css } from '@emotion/native';
 
-import { Color, Palette, SizeButton } from '@/types/theme';
-
-import { Text } from '../Text/Text';
+import { Color, Palette } from '@/types/theme';
 
 export type ButtonProps = {
   color?: Color;
   elevation?: boolean;
   palette?: Palette;
-  size?: SizeButton;
 };
 
 const buttonModifiers = {
@@ -23,49 +19,15 @@ const buttonModifiers = {
 };
 
 export const Button = styled.TouchableOpacity<ButtonProps>`
-  ${({
-    theme,
-    elevation = false,
-    palette = 'primary',
-    color = 'main',
-    size = 'medium',
-  }) => css`
+  ${({ theme, elevation = false, palette = 'primary', color = 'main' }) => css`
     align-items: center;
-    background-color: ${theme.colors.default[palette][color]};
-    border-radius: ${theme.rfvalue(30)}px;
+    background-color: ${theme.colors[palette][color]};
+    border-radius: 30px;
     flex-direction: row;
-    height: ${theme.size.button[size]}px;
-    justify-content: center;
+    height: 48px;
     justify-content: space-evenly;
     width: 100%;
+
     ${elevation && buttonModifiers.elevation()}
-  `}
-`;
-
-export const Label = styled(Text.Subtitle).attrs((props) => ({
-  color: 'main',
-  palette: 'white',
-  size: 'medium',
-  ...props,
-}))`
-  ${({ theme }) => css`
-    letter-spacing: ${theme.rfvalue(1)}px;
-  `}
-`;
-
-export const IconAccount = styled(Image).attrs<ImageProps>({
-  contentFit: 'contain',
-})`
-  ${({ theme }) => css`
-    height: ${theme.rfvalue(30)}px;
-    width: ${theme.rfvalue(30)}px;
-  `}
-`;
-
-export const ButtonIcon = styled(Button)`
-  ${({ theme }) => css`
-    border-radius: ${theme.rfvalue(10)}px;
-    height: ${theme.rwvalue(38)}px;
-    width: ${theme.rwvalue(38)}px;
   `}
 `;
