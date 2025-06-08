29e101432de1a506343cf3a444f9929ef420e4d6
Emilio Correa <ehcorrea60@gmail.com>
2025-06-08 17:17:35 -0300

    refactor: update TabBar and Children syles

--- src/components/TabBar/TabBar.styles.ts
+++ src/components/TabBar/TabBar.styles.ts
@@ -1,26 +1,26 @@
-import styled, { css } from 'styled-components/native';
+import styled, { css } from '@emotion/native';
 
 type ContainerProps = {
   bottom?: number;
 };
 
 export const Container = styled.View<ContainerProps>`
-  ${({ theme, bottom }) => css`
+  ${({ theme }) => css`
     align-items: center;
     align-self: center;
-    background-color: ${theme.colors.default.white.main};
+    background-color: ${theme.colors.white.main};
+    border-top-left-radius: ${theme.rhvalue(24)}px;
+    border-top-right-radius: ${theme.rhvalue(24)}px;
+    bottom: 0;
     elevation: 3;
     flex-direction: row;
-    top: 0;
-    bottom: 0;
     justify-content: space-around;
     padding: ${theme.rhvalue(20)}px;
-    shadow-color: ${theme.colors.default.black.main};
+    shadow-color: ${theme.colors.black.main};
     shadow-offset: 0px 1px;
     shadow-opacity: 0.2;
     shadow-radius: 3px;
+    top: 0;
     width: 100%;
-    border-top-left-radius: ${theme.rhvalue(24)}px;
-    border-top-right-radius: ${theme.rhvalue(24)}px;
   `}
 `;
