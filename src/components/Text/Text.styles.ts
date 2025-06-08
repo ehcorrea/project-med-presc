90f7ba82c0431231d8a19cf13cc9b4fc2a05439e
Emilio Correa <ehcorrea60@gmail.com>
2025-06-07 21:14:48 -0300

    refactor: update Text styles

--- src/components/Text/Text.styles.ts
+++ src/components/Text/Text.styles.ts
@@ -1,8 +1,10 @@
+import styled, { css } from '@emotion/native';
 import { Color, FontSize, FontWeight, Palette } from '@/types/theme';
-import styled, { css } from 'styled-components/native';
 
 export type TextProps = {
   color?: Color;
+  customSize?: number;
+  height?: number;
   palette?: Palette;
   size?: FontSize;
   weight?: FontWeight;
@@ -10,14 +12,27 @@ export type TextProps = {
 
 export const Text = styled.Text<TextProps>`
   ${({
-    theme,
     color = 'main',
-    palette = 'black',
+    customSize,
+    height,
+    palette = 'primary',
     size = 'medium',
+    theme,
     weight = 'regular',
   }) => css`
-    color: ${theme.colors.default[palette][color]};
-    font-family: ${theme.font.weight[weight]};
-    font-size: ${theme.font.size[size]}px;
+    color: ${theme.colors[palette][color]};
+    font-family: ${theme.fonts.weight[weight]};
+    font-size: ${theme.fonts.size[size]}px;
+    line-height: ${theme.fonts.height[size]}px;
+
+    ${height &&
+    css`
+      height: ${theme.rfvalue(height)}px;
+    `}
+
+    ${customSize &&
+    css`
+      font-size: ${theme.rfvalue(customSize)}px;
+    `}
   `}
 `;
