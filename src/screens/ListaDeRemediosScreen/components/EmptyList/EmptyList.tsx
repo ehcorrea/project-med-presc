44e98d3195a395a5a93451aa2f078d199d9547ad
Emilio Correa <ehcorrea60@gmail.com>
2025-06-08 22:33:46 -0300

    refactor: update EmptyList image and styles

--- src/screens/ListaDeRemediosScreen/components/EmptyList/EmptyList.tsx
+++ src/screens/ListaDeRemediosScreen/components/EmptyList/EmptyList.tsx
@@ -1,22 +1,14 @@
 import { Image } from 'expo-image';
-import { useWindowDimensions, View } from 'react-native';
-
-import { Spancing, Text } from '@/components';
+import { View } from 'react-native';
 
 export function EmptyList() {
-  const { width } = useWindowDimensions();
-
   return (
-    <View className="flex-1 items-center p-[5%]">
+    <View className="items-center w-full justify-center aspect-square">
       <Image
         contentFit="contain"
-        style={{ width: width / 2, height: width / 2 }}
-        source={require('@/assets/images/list.svg')}
+        className="h-full w-full"
+        source={require('@/assets/images/list.png')}
       />
-      <Spancing y={4} />
-      <Text size="large" className="text-center">
-        Parece que você ainda {`\n`}não tem nada cadastrado...
-      </Text>
     </View>
   );
 }
