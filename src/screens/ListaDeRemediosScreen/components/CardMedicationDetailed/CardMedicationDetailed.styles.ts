b412e3490b5ca73ea78d86cb968c567b4d0f394b
Emilio Correa <ehcorrea60@gmail.com>
2025-08-10 11:49:16 -0300

    refactor: add emotion to CardMedicationDetailed

--- src/screens/ListaDeRemediosScreen/components/CardMedicationDetailed/CardMedicationDetailed.styles.ts
+++ src/screens/ListaDeRemediosScreen/components/CardMedicationDetailed/CardMedicationDetailed.styles.ts
@@ -1,24 +1,15 @@
 import { Shadow as ShadowRN } from 'react-native-shadow-2';
-import styled, { css } from 'styled-components/native';
-import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import styled, { css } from '@emotion/native';
 
-export const Shadow = styled(ShadowRN).attrs({
-  corners: { topEnd: false, topStart: false },
-  distance: 3,
-  sides: { top: false },
-  startColor: '#00000010',
-  stretch: true,
-})`
-  ${({ theme }) => css`
-    flex: 1;
-    border-radius: ${theme.rfvalue(10)}px;
-  `}
+export const Shadow = styled(ShadowRN)`
+  border-radius: 10px;
+  flex: 1;
 `;
 
 export const Container = styled.View`
   ${({ theme }) => css`
-    background-color: ${theme.colors.default.white.main};
-    border-radius: ${theme.rfvalue(10)}px;
+    background-color: ${theme.colors.white.main};
+    border-radius: 10px;
     overflow: hidden;
   `}
 `;
@@ -38,26 +29,6 @@ export const Header = styled.View<HeaderProps>`
   `}
 `;
 
-type IconProps = {
-  name?: string;
-};
-
-export const IconType = styled(MaterialCommunityIcons).attrs<IconProps>(
-  ({ name, theme }) => ({
-    size: theme.rfvalue(20),
-    color: theme.colors.default.white.main,
-    name,
-  })
-)``;
-
-export const IconOptions = styled(MaterialCommunityIcons).attrs<IconProps>(
-  ({ theme }) => ({
-    color: theme.colors.default.white.main,
-    name: 'dots-horizontal',
-    size: theme.rfvalue(30),
-  })
-)``;
-
 export const Body = styled.View`
   ${({ theme }) => css`
     flex-direction: row;
@@ -65,29 +36,11 @@ export const Body = styled.View`
   `}
 `;
 
-export const IconDot = styled(Ionicons).attrs<IconProps>(
-  ({ color, theme, ...props }) => ({
-    color,
-    size: theme.rfvalue(24),
-    ...props,
-  })
-)``;
-
 export const Footer = styled.View`
   ${({ theme }) => css`
     align-items: center;
     flex-direction: row;
     justify-content: space-between;
     padding: ${theme.rfvalue(16)}px;
-  `}
-`;
-
-export const IconTimer = styled(Ionicons).attrs<IconProps>(({ theme }) => ({
-  color: theme.colors.default.error[80],
-  name: 'time-outline',
-  size: theme.rfvalue(14),
-}))`
-  ${({ theme }) => css`
-    margin-top: ${theme.rfvalue(3)}px;
   `}
 `;
